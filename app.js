b9f4183cf6b48c25e647c1a4c20c5116c00132d2
Anto <goubinou@hotmail.fr>
2024-04-15 21:38:23 +0200

    mongodb connection & gitIgnore

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ var fs = require('fs');
 var app = express();
 var PORT = 8080;
 
-// http.createServer(function (req, res) {
-//   fs.readFile('page.html', function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     return res.end();
-//   });
-// }).listen(8080);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Admin:nimda@golfsgapp2024.09jxwl8.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	fs.readFile('page.html', function(err, data) {
